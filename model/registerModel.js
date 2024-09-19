4839ae934e34c7358b74748dc7f9d82c780a6f39
vengateshlikes <126547084+vengateshlikes@users.noreply.github.com>
2024-09-19 14:15:40 +0530

    backend host last preview

--- model/registerModel.js
+++ model/registerModel.js
@@ -11,7 +11,7 @@ const registrationSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    dob: {
+    dob: {  
         type: Date,
         required: true
     },
